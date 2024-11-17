b01bd814b399287a64c0acb4dfb00f2e986675fd
Kacper Wyczawski <97523625+kacperwyczawski@users.noreply.github.com>
2024-11-17 12:45:44 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,10 +18,10 @@ export default {
 			"lemonade",
 			{
 				lime: {
-					primary: "#a3e635",
-					secondary: "#a3e635",
-					accent: "#a3e635",
-					neutral: "#1c1c1e",
+					primary: "#60b45a",
+					secondary: "#ffee8c",
+					accent: "#60b45a",
+					neutral: "#ffee8c",
 					"base-100": "#0d0d0d",
 					"base-200": "#1c1c1e",
 					"base-300": "#2a2a2c",
