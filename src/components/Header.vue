04a8c40bb02e45af676b05e6283852668abea37d
Kacper Wyczawski <97523625+kacperwyczawski@users.noreply.github.com>
2024-11-17 09:06:54 +0100

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@ const commitHash = __COMMIT_HASH__;
       <a
         href="https://github.com/kacperwyczawski/autocut/commits/main/"
         class="badge"
-        >{{ commitHash }}</a
+        >EKOLOGICZNA UTYLIZACJA MATERIAŁÓW DRUKARSKICH</a
       >
     </div>
     <div class="navbar-end hidden md:flex gap-2">
